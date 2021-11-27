9ba502bc2e90c10bb463240f8d7183ac696925da
Vladimir Pavković <vladimirpavk@gmail.com>
2021-11-27 18:19:43 +0100

    +

--- src/components/PopulateDatabase/PopulateDatabase.js
+++ src/components/PopulateDatabase/PopulateDatabase.js
@@ -1,34 +1,34 @@
 import { collection, addDoc } from 'firebase/firestore';
 
-import Users from '../../assets/import';
 import db from '../../db/db';
+
+import { randomUsers } from '../../assets/randomUsers';
+
+const getRandomInt = (max, base) => {
+  return Math.floor(Math.random() * max) + base;
+}
 
 const PopulateDatabase = (props)=>{
     const populateDb = ()=>{
-      /*   Users.forEach(user => {
-            //console.log(user)      ;
-          fetch('https://meals-f92cb-default-rtdb.europe-west1.firebasedatabase.app/users.json', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(user)
-          }).then(
-            (response)=>response.json()
-          ).then(
-            data=>console.log(data)
-          ).catch(
-            (ex)=>console.log('Error', ex)
-          )});     */
-/*           Users.forEach(user=>{
-            addDoc(collection(db, "users"), user).then(doc=>console.log(doc)).catch(e=>console.error(e))
-          }); */
-          
-         /*  Users.forEach(user=>{
-            addDoc(collection(db, "randomUsers"), user).then(doc=>console.log(doc)).catch(e=>console.error(e))
-          }); */
+          const messedUsers = randomUsers.map(user=>{
+            let newGenList = [];
+             for(let y=0; y<getRandomInt(3, 1); y++){
+               let newRandomNumber = getRandomInt(9, 0);
+               while(newGenList.indexOf(newRandomNumber)!==-1) newRandomNumber = getRandomInt(9, 0);
+               newGenList.push(newRandomNumber);
+             }
+             return {
+               auth: newGenList,
+               password: 'proba1pass',
+               ...user
+             }
+          });
 
-          
+         //console.log(messedUsers);
+
+        messedUsers.forEach(user=>{
+          addDoc(collection(db, "randomUsers"), user).then(doc=>console.log(doc)).catch(e=>console.error(e))
+        });
     }
     return(
         <button onClick={populateDb}>Populate</button>
